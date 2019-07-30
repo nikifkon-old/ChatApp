11b4efe18a49ab138f6c97c440b4d8623437b7e5
Kostya Nikiforov <kostya.nik.3854@gmail.com>
2019-07-30 13:45:57 +0500

    add Profile model

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
 
+media = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+static = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path('^', TemplateView.as_view(template_name="index.html")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('auth/', include('djoser.urls')),
+    re_path('(^$)|(login)|(get-started)', 
+        TemplateView.as_view(template_name="index.html")),
+
+] + media + static
