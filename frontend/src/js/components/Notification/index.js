3008d8b4d500e58d0c85b71d152c9461a93fbd0d
Kostya Nikiforov <kostya.nik.3854@gmail.com>
2019-08-04 14:08:21 +0500

    add Notification component

---
+++ frontend/src/js/components/Notification/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { NotificationContainer } from './styles'
+import { P } from '../../styles'
+
+const Notification = ({message, type}) => {
+    return (
+        <NotificationContainer type={type}>
+          <P>{message}</P>
+        </NotificationContainer>
+    )
+}
+
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+  type: PropTypes.string
+}
+
+Notification.defaultProps = {
+  type: "error"
+}
+
+export default Notification
