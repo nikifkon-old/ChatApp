d08c4901bacb8285639dd714c1749c2709c52413
Kostya Nikiforov <kostya.nik.3854@gmail.com>
2019-09-07 15:34:28 +0500

    dialog list service

--- frontend/src/js/services/userDataService.js
+++ frontend/src/js/services/userDataService.js
@@ -11,3 +11,49 @@ export const getUserDataService = user_id => {
 
   return axios(LOGIN_API_ENDPOINT, headers)
 }
+
+{/*
+  list of
+{
+    "id": 1,
+    "members": [
+        {
+          user_id,
+          user,
+          avatar,
+        }
+    ],
+    "last_message": {
+        "sender": 1,
+        "text": "123",
+        "date": "2019-08-12"
+    }
+}*/}
+export const getDialogsDataService = user_id => {
+  const GET_DIALOG_API_ENDPOINT = `${BASE_URL}/api/v1/dialog/?user_id=${user_id}`
+  const headers = {
+    'headers': {
+      ...getAuthHeaders()
+    }
+  }
+  return axios(GET_DIALOG_API_ENDPOINT, headers)
+}
+
+// list of
+// {
+//     "id": 1,
+//     "sender": 1,
+//     "dialog": 1,
+//     "text": "123",
+//     "date": "2019-08-12"
+// },
+export const getMessagesInDialogService = dialog_id => {
+  const GET_MESSAGES_IN_DIALOGS_API_ENDPOINT
+   = `${BASE_URL}/api/v1/message/dialog/?dialog=${dialog_id}`
+  const headers = {
+    'headers': {
+      ...getAuthHeaders()
+    }
+  }
+  return axios(GET_MESSAGES_IN_DIALOGS_API_ENDPOINT, headers)
+}
