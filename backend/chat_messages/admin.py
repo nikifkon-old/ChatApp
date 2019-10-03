3e7a6eaddb4f07d0bbbc324b9224d0c636be87ad
Kostya Nikiforov <kostya.nik.3854@gmail.com>
2019-10-03 17:18:22 +0500

    message mixin

--- backend/chat_messages/admin.py
+++ backend/chat_messages/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import GroupMessage, DialogMessage, GroupMessageInfo
+from .models import (
+    GroupMessage,
+    DialogMessage,
+    GroupMessageInfo,
+    DialogMessageInfo
+)
 
 admin.site.register(GroupMessage)
 admin.site.register(DialogMessage)
+admin.site.register(DialogMessageInfo)
 admin.site.register(GroupMessageInfo)
--- backend/chat_messages/models.py
+++ backend/chat_messages/models.py
@@ -4,22 +4,38 @@ from backend.groups.models import ChatGroup
 from backend.dialogs.models import Dialog
 
 
-class GroupMessage(models.Model):
-    """ Group message """
+class MessageMixin(models.Model):
+    """ Message Mixin"""
     sender = models.ForeignKey(
-        Profile, on_delete=models.CASCADE,
-        related_name="sended_group_messages"
+        Profile,
+        on_delete=models.CASCADE,
+        related_name="%(class)s_sended"
     )
-    group = models.ForeignKey(ChatGroup, on_delete=models.CASCADE, related_name="messages")
     text = models.TextField(max_length=1000)
-    date = models.DateTimeField("date of created or updated", auto_now=True)
+    date = models.DateTimeField(
+        "date of created or updated",
+        auto_now=True
+    )
+
+    class Meta:
+        abstract = True
+
+
+class GroupMessage(MessageMixin):
+    """ Group message """
+    group = models.ForeignKey(
+        ChatGroup,
+        on_delete=models.CASCADE,
+        related_name="messages"
+    )
     readers = models.ManyToManyField(
         Profile,
         through="GroupMessageInfo",
-        related_name="group_messages"
+        related_name=f"group_messages"
     )
 
     def save(self, *args, **kwargs):
+        """ set readers as group members """
         super().save(*args, **kwargs)
         for person in self.group.members.all():
             unread = True
@@ -53,10 +69,29 @@ class GroupMessageInfo(models.Model):
 
 class DialogMessage(models.Model):
     """ Dialog message """
-    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="dialog_messages")
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="sended_messages")
     dialog = models.ForeignKey(Dialog, on_delete=models.CASCADE, related_name="messages")
     text = models.TextField(max_length=1000)
     date = models.DateTimeField("date of created or updated", auto_now=True)
+    readers = models.ManyToManyField(
+        Profile,
+        through="DialogMessageInfo",
+        related_name="dialog_messages"
+    )
+
+    def save(self, *args, **kwargs):
+        """ set readers as dialog members """
+        super().save(*args, **kwargs)
+        for person in self.dialog.members.all():
+            unread = True
+            if person.id == self.sender.id:
+                unread = False
+
+            DialogMessageInfo.objects.create(
+                message=self,
+                person=person,
+                unread=unread,
+            )
 
     class Meta:
         verbose_name = "Message in dialog"
@@ -64,3 +99,14 @@ class DialogMessage(models.Model):
 
     def __str__(self):
         return f"{self.sender} - {self.dialog.id}"
+
+
+class DialogMessageInfo(models.Model):
+    """ m2m for profile & dialog message """
+    message = models.ForeignKey(DialogMessage, on_delete=models.CASCADE)
+    person = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    unread = models.BooleanField(default=True)
+    stared = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"message {self.message.id}, for '{self.person}' user"
