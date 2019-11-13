3ca22ceb6acbd543138b1e055b32a7449a79ef95
Kostya Nikiforov <kostya.nik.3854@gmail.com>
2019-11-13 17:18:32 +0500

    feat(Redux): add group actions and group events

--- frontend/src/js/reducers/ChatApp/index.js
+++ frontend/src/js/reducers/ChatApp/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux'
 import headerReducer from './headerReducer'
 import dialogsReducer from './dialogsReducer'
+import groupsReducer from './groupReducer'
 import websocketReducer from './websocketReducer'
 
 const AppReducer = combineReducers({
   header: headerReducer,
   dialogs: dialogsReducer,
+  groups: groupsReducer,
   websocket: websocketReducer,
 })
 
