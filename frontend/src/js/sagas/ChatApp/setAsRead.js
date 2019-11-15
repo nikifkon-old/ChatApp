ed7c88f814338de6111c216062e082168dc612c0
Kostya Nikiforov <kostya.nik.3854@gmail.com>
2019-11-15 14:35:19 +0500

    fix: add set as read saga for group

--- frontend/src/js/sagas/ChatApp/setAsRead.js
+++ frontend/src/js/sagas/ChatApp/setAsRead.js
@@ -18,6 +18,21 @@ function* setAsRead({payload}) {
   })
 }
 
+function* setAsReadGroupMessage({payload}) {
+  const { chat_id, message_id } = payload
+  yield put({
+    type: types.WEBSOCKET_SEND_REQUEST,
+    payload: {
+      event: events.GROUP_UPDATE_MESSAGE, 
+      data: {
+        id: message_id,
+        unread: false,
+      }
+    }
+  })
+}
+
 export default function* () {
   yield takeEvery(types.SET_AS_READ_DIALOG_MESSAGE, setAsRead)
+  yield takeEvery(types.SET_AS_READ_GROUP_MESSAGE, setAsReadGroupMessage)
 }
