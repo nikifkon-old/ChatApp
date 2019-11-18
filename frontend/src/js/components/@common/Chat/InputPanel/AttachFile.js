8b975e09d3ab19efc78675dbee0a633c89b15644
Kostya Nikiforov <kostya.nik.3854@gmail.com>
2019-11-18 14:58:46 +0500

    feat(Popover): replace Popover to Menu.

--- frontend/src/js/components/@common/Chat/InputPanel/AttachFile.js
+++ frontend/src/js/components/@common/Chat/InputPanel/AttachFile.js
@@ -1,47 +1,60 @@
-import React from 'react'
+import React, { useState, Fragment } from 'react'
 import PropTypes from 'prop-types'
+import { Menu } from '@material-ui/core'
 
 import { IconButton, Popover } from '../../../index'
 import { ContentGrid } from '../../../../styles'
 
 function AttachFile() {
+  const [anchorEl, setAnchorEl] = useState(null)
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
-    <Popover
-      transformOrigin={{
-        vertical: 'bottom',
-        horizontal: 'center'
-      }}
-      button={
-        ({handleClick}) => (
+    <Fragment>
+      <IconButton
+        icon="attach_file"
+        onClick={handleClick}
+      />
+      <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "center"
+        }}
+        transformOrigin={{
+          vertical: "bottom",
+          horizontal: "center"
+        }}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <ContentGrid
+          container
+          alignItems="center"
+        >
           <IconButton
-            icon="attach_file"
-            onClick={handleClick}
+            icon="photo_camera"
+            borderRadius="0"
           />
-        )
-      }
-      modal={
-        () => (
-          <ContentGrid
-            container
-            alignItems="center"
-          >
-            <IconButton
-              icon="photo_camera"
-              borderRadius="0"
-            />
-            <IconButton
-              icon="videocam"
-              borderRadius="0"
-            />
-            <IconButton
-              icon="music_note"
-              borderRadius="0"
-            />
-          </ContentGrid>
-        )
-      }
-    />
-);
+          <IconButton
+            icon="videocam"
+            borderRadius="0"
+          />
+          <IconButton
+            icon="music_note"
+            borderRadius="0"
+          />
+        </ContentGrid>
+      </Menu>
+    </Fragment>
+  );
 }
 
 export default AttachFile;
--- frontend/src/js/components/@common/Chat/TopPanel.js
+++ frontend/src/js/components/@common/Chat/TopPanel.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import { StyledTopPanel } from './styles'
 import { IconButton } from '../../index'
 import { GridItem, P, H4 } from '../../../styles'
+import { Menu, MenuItem } from '@material-ui/core'
 
 function TopPanel(props) {
-  const { title } = props
+  const { title, deleteChat, id } = props
+  const [anchorEl, setAnchorEl] = useState(null)
+  
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const handleDelete = () => {
+    deleteChat({id})
+  }
 
   return (
     <StyledTopPanel>
@@ -34,13 +48,31 @@ function TopPanel(props) {
         component={IconButton}
         icon="more_vert"
         size="small"
+        onClick={handleClick}
       />
+      <Menu
+        open={Boolean(anchorEl)}
+        anchorEl={anchorEl}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right"
+        }}
+        onClose={handleClose}
+      >
+        <MenuItem
+          onClick={handleDelete}
+        >
+          Delete chat
+        </MenuItem>
+      </Menu>
     </StyledTopPanel>
   )
 }
 
 TopPanel.propTypes = {
   title: PropTypes.string,
+  deleteChat: PropTypes.func.isRequired,
+  id: PropTypes.number,
 }
 
 export default TopPanel
