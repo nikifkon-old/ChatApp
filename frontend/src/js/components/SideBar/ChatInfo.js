ac462d5f1ae415e70bd771a61489c9dcf50bdc0e
Kostya Nikiforov <kostya.nik.3854@gmail.com>
2019-11-21 17:04:43 +0500

    refactoring(ChatInfo): delete decorate elements

--- frontend/src/js/components/SideBar/ChatInfo.js
+++ frontend/src/js/components/SideBar/ChatInfo.js
@@ -1,12 +1,9 @@
 import React, { Fragment } from 'react'
-import { Grid, IconButton as MaterialIconButton } from '@material-ui/core'
 import PropTypes from 'prop-types'
 
 import { IconButton } from '../index'
-import ChatLogo from '../../../assets/logo.png'
 import DefaultAvatar from '../../../assets/defaultAvatar.jpg'
 import { ContentGrid, H1, P, Img } from '../../styles'
-import { StyledChatLogo } from './styles'
 
 function ChatInfo({data}) {
   if(!data.avatar) {
@@ -18,23 +15,19 @@ function ChatInfo({data}) {
   return (
     <Fragment>
       <ContentGrid container
-        justify="space-between"
+        justify="flex-end"
         alignItems="center"
       >
-        <MaterialIconButton size="small">
-          <StyledChatLogo src={ChatLogo} width="20px" alt="Logo"/>
-        </MaterialIconButton>
         <IconButton icon="more_horiz" size="small" />
       </ContentGrid>
 
-      <Grid container
+      <ContentGrid container
         direction="column"
         alignItems="center"
       >
         <Img src={data.avatar} width="150px" round alt="avatar"/>
         <H1>{data.name}</H1>
-        <P noMargin>location</P>
-      </Grid>
+      </ContentGrid>
     </Fragment>
   );
 }
