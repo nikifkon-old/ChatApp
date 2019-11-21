ac9c39a84ccaa3d862008e50195f0b3cb632ba92
Kostya Nikiforov <kostya.nik.3854@gmail.com>
2019-11-21 16:03:45 +0500

    refactoring(Menu): remove don't exist links

--- frontend/src/js/containers/Menu/Menu.js
+++ frontend/src/js/containers/Menu/Menu.js
@@ -18,22 +18,23 @@ function Menu() {
         color={dark_cont1}
         height="2px"
       />
-    <MenuItem id={1} title="All Messages" link="messages" unreadCount={dialogsUnread} />
+    <MenuItem id={1} title="Dialogs" link="messages" unreadCount={dialogsUnread} />
       <MenuItem id={2} title="Unread" link="messages?filter=unread" unreadCount={0} />
       <MenuItem id={3} title="Stared" link="messages?filter=stared" unreadCount={0} />
-      <MenuItem id={4} title="Create room" link="create" />
       <ColoredLine
         color={dark_cont1}
         height="2px"
       />
       <MenuItem id={5} title="Groups" unreadCount={groupsUnread} />
-      <MenuItem id={6} title="Channels" unreadCount={18} />
       <ColoredLine
         color={dark_cont1}
         height="2px"
       />
-      <MenuItem id={7} title="Media" unreadCount={0} />
-      <MenuItem id={8} title="Help" />
+      <MenuItem id={4} title="Create room" link="create" />
+      <ColoredLine
+        color={dark_cont1}
+        height="2px"
+      />
       <MenuItem id={9} title="Settings" />
     </StyledChatMenu>
   );
