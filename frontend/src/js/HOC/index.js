999858e1fc1fef9b853da575f15ca5ec3ec427b0
Kostya Nikiforov <kostya.nik.3854@gmail.com>
2019-11-21 16:41:26 +0500

    refactoring: delete unused HOC ChatInfo

--- frontend/src/js/HOC/index.js
+++ frontend/src/js/HOC/index.js
@@ -1,11 +1,9 @@
 import Private from './Private'
 import AuthRouter from './AuthRouter'
 import withHeaderStatus from './withHeaderStatus'
-import withChatInfo from './withChatInfo'
 
 export {
   Private,
   AuthRouter,
   withHeaderStatus,
-  withChatInfo,
 }
