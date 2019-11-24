c4dcbe2790050a6e852d2d2c62777a6f39b52bf7
Kostya Nikiforov <kostya.nik.3854@gmail.com>
2019-11-24 15:57:13 +0500

    feat(messageReducer): push notification on websocket receive message

--- frontend/src/js/sagas/Websocket/messageReducer.js
+++ frontend/src/js/sagas/Websocket/messageReducer.js
@@ -4,221 +4,184 @@ import * as types from '../../actions'
 import * as events from '../../actions/websocketEvents'
 
 function* MessageReducer({payload: event}) {
-  switch (event.event) {
-    case events.AUTHENTICATE:
-      if (event.status === 'ok') {
-        console.log('%c%s', 'color: green;', 'authenticate success')
-        yield put({
-          type: types.WEBSOCKET_CONNECT_SUCCESS,
-          payload: {
-            response: {
-              ...event.data
-            }
+  if (event.event === events.AUTHENTICATE) {
+    if (event.status === 'ok') {
+      console.log('%c%s', 'color: green;', 'authenticate success')
+      yield put({
+        type: types.WEBSOCKET_CONNECT_SUCCESS,
+        payload: {
+          response: {
+            ...event.data
           }
-        })
-      } else {
-        yield put({
-          type: types.WEBSOCKET_CONNECT_FAILURE,
-          payload: {
-            response: {
-              ...event.data,
-              status: 401
-            }
+        }
+      })
+    } else {
+      yield put({
+        type: types.WEBSOCKET_CONNECT_FAILURE,
+        payload: {
+          response: {
+            ...event.data,
+            status: 401
           }
-        })
-        console.log('%c%s', 'color: red;', event.data.detail);
-      }
-      break
+        }
+      })
+    }
+  }
 
-    case events.DIALOGS_LIST:
-      if(event.status === 'ok') {
+  else if (event.status === 'ok') {
+    switch (event.event) {
+      case events.DIALOGS_LIST:
         yield put({
           type: types.SET_DIALOGS_DATA,
           payload: event.data
         })
-      } else {
-        console.log(event.data.detail);
-        yield put({
-          type: types.GET_DIALOGS_FAILURE,
-          payload: event.data.detail
-        })
-      }
-      break
+        break
 
-    case events.GROUPS_LIST:
-      if(event.status === 'ok') {
+      case events.GROUPS_LIST:
         yield put({
           type: types.SET_GROUPS_DATA,
           payload: event.data
         })
-      } else {
-        console.log(event.data.detail);
-        yield put({
-          type: types.GET_GROUPS_FAILURE,
-          payload: event.data.detail
-        })
-      }
+        break
 
-    case events.DIALOG_GET:
-      if(event.status === 'ok') {
+      case events.DIALOG_GET:
         yield put({
           type: types.SET_DIALOG_DATA,
           payload: event.data
         })
-      } else {
-        console.log(event.data.detail);
-        yield put({
-          type: types.GET_DIALOGS_FAILURE,
-          payload: event.data.detail
-        })
-      }
-      break
+        break
 
-    case events.GROUP_GET:
-      if(event.status === 'ok') {
+      case events.GROUP_GET:
         yield put({
           type: types.SET_GROUP_DATA,
           payload: event.data
         })
-      } else {
-        console.log(event.data.detail);
-        yield put({
-          type: types.GET_GROUPS_FAILURE,
-          payload: event.data.detail
-        })
-      }
-      break
+        break
 
-    case events.DIALOG_SEND:
-      if (event.status === 'ok') {
+      case events.DIALOG_SEND:
         yield put({
           type: types.PUSH_RECEIVE_MESSAGE_IN_DIALOG,
           payload: event.data
         })
-      } else {
-        console.log(event.data.detail);
-      }
-      break
+        break
 
-    case events.GROUP_SEND:
-      if (event.status === 'ok') {
+      case events.GROUP_SEND:
         yield put({
           type: types.PUSH_RECEIVE_MESSAGE_IN_GROUP,
           payload: event.data
         })
-      } else {
-        console.log(event.data.detail);
-      }
-      break
+        break
 
-    case events.DIALOG_DELETE_MESSAGE:
-      if (event.status === 'ok') {
+      case events.DIALOG_DELETE_MESSAGE:
+        yield put({
+          type: types.PUSH_NOTIFICATION,
+          payload: 'Message deleted successfully'
+        })
         yield put({
           type: types.DELETE_DIALOG_MESSAGE,
           payload: event.data
         })
-      } else {
-        console.log(event.data.detail);
-      }
-      break
+        break
 
-    case events.GROUP_DELETE_MESSAGE:
-      if (event.status === 'ok') {
+      case events.GROUP_DELETE_MESSAGE:
+        yield put({
+          type: types.PUSH_NOTIFICATION,
+          payload: 'Message deleted successfully'
+        })
         yield put({
           type: types.DELETE_GROUP_MESSAGE,
           payload: event.data
         })
-      } else {
-        console.log(event.data.detail);
-      }
-      break
+        break
 
-    case events.DIALOG_UPDATE_MESSAGE:
-      if (event.status === 'ok') {
+      case events.DIALOG_UPDATE_MESSAGE:
+        yield put({
+          type: types.PUSH_NOTIFICATION,
+          payload: 'Message updated successfully'
+        })
         yield put({
           type: types.UPDATE_DIALOG_MESSAGE,
           payload: event.data
         })
-      } else {
-        console.log(event.data.detail);
-      }
-      break
+        break
 
-    case events.GROUP_UPDATE_MESSAGE:
-      if (event.status === 'ok') {
+      case events.GROUP_UPDATE_MESSAGE:
+        yield put({
+          type: types.PUSH_NOTIFICATION,
+          payload: 'Message updated successfully'
+        })
         yield put({
           type: types.UPDATE_GROUP_MESSAGE,
           payload: event.data
         })
-      } else {
-        console.log(event.data.detail);
-      }
-      break
+        break
 
-    case events.DIALOG_CREATE:
-      if (event.status === 'ok') {
+      case events.DIALOG_CREATE:
+        yield put({
+          type: types.PUSH_NOTIFICATION,
+          payload: 'Dialog created successfully'
+        })
         yield put({
           type: types.PUSH_NEW_DIALOG,
           payload: event.data
         })
-      } else {
-        console.log(event.data.detail);
-      }
-      break
-      
-    case events.GROUP_CREATE:
-      if (event.status === 'ok') {
+        break
+        
+      case events.GROUP_CREATE:
+        yield put({
+          type: types.PUSH_NOTIFICATION,
+          payload: 'Group created successfully'
+        })
         yield put({
           type: types.PUSH_NEW_GROUP,
           payload: event.data
         })
-      } else {
-        console.log(event.data.detail);
-      }
-      break
-    
-    case events.DIALOG_DELETE:
-      if (event.status === 'ok') {
+        break
+      
+      case events.DIALOG_DELETE:
+        yield put({
+          type: types.PUSH_NOTIFICATION,
+          payload: 'Dialog delete successfully'
+        })
         yield put({
           type: types.POP_DIALOG,
           payload: event.data
         })
-      } else {
-        console.log(event.data.detail);
-      }
-      break
-    
-    case events.GROUP_DELETE:
-      if (event.status === 'ok') {
+        break
+      
+      case events.GROUP_DELETE:
+        yield put({
+          type: types.PUSH_NOTIFICATION,
+          payload: 'Group delete successfully'
+        })
         yield put({
           type: types.POP_GROUP,
           payload: event.data
         })
-      } else {
-        console.log(event.data.detail);
-      }
-      break
-    
-    case events.DIALOG_MESSAGE_STAR:
-      if (event.status === 'ok') {
+        break
+      
+      case events.DIALOG_MESSAGE_STAR:
         yield put({
           type: types.UPDATE_DIALOG_MESSAGE,
           payload: event.data
         })
-      }
-      break
+        break
 
-    case events.GROUP_MESSAGE_STAR:
-      if (event.status === 'ok') {
+      case events.GROUP_MESSAGE_STAR:
         yield put({
           type: types.UPDATE_GROUP_MESSAGE,
           payload: event.data
         })
-      }
-      break
+        break
 
-    default:
-      yield
+      default:
+        yield
+    }
+  } else {
+    yield put({
+      type: types.PUSH_NOTIFICATION,
+      payload: event.data.detail
+    })
   }
 }
 
