db2852ee849f2bcdd4129351a912998891429ea1
Kostya Nikiforov <kostya.nik.3854@gmail.com>
2020-06-15 13:31:38 +0500

    added mixin for views

--- backend/dialogs/views.py
+++ backend/dialogs/views.py
@@ -7,91 +7,42 @@ from backend.api.v1.users.serializers import UserSerializer
 from backend.api.v1.dialogs.serializers import DialogSerializer, DialogMessageSerializer, LastMessageSerizalizer
 from backend.dialogs.models import Dialog, DialogMessage, DialogMessageInfo, DialogMembership
 from backend.dialogs.forms import DialogForm, DialogMessageForm
+from backend.socket_chat.mixins.view import ChatViewMixin
 
 
 User = get_user_model()
 
 
-class DialogView:
-    user_id = None
-
-    def set_user_id(self, user_id: int):
-        self.user_id = user_id
+class DialogView(ChatViewMixin):
+    class Meta:
+        chat_model = Dialog
+        chat_serializer = DialogSerializer
+        chat_form = DialogForm
+        chat_message_model = DialogMessage
+        chat_message_serializer = DialogMessageSerializer
+        chat_message_form = DialogMessageForm
+        chat_membership_model = DialogMembership
+        chat_message_info_model = DialogMessageInfo
+        last_message_serializer = LastMessageSerizalizer
+        chat_name = "dialog"
 
     @database_sync_to_async
     def get(self, id_: int, with_messages: bool = False, filter_: str = None, user_id: int = None) -> dict:
         if user_id is None:
             user_id = self.user_id
+        data = async_to_sync(super().get)(id_, with_messages, filter_, user_id)
+
         dialog = Dialog.objects.get(id=id_)
-        data = DialogSerializer(dialog).data
-
-        last_message = self._get_last_message(dialog)
-        data["last_message"] = LastMessageSerizalizer(last_message).data
-
-        data["unread_count"] = self._get_unread_count(dialog, user_id=user_id)
-
         interlocutor = self._get_interlocutor(dialog, user_id=user_id)
         data["interlocutor"] = UserSerializer(interlocutor).data
 
-        messages = []
-        if with_messages:
-            messages = self._get_dialog_messages_with_filter(dialog, filter_=filter_)
-        data["messages"] = self._get_serialized_message(messages, many=True, user_id=user_id)
         return data
-
-    def _get_last_message(self, dialog: Dialog) -> DialogMessage:
-        return dialog.messages.last()
-
-    def _get_unread_count(self, dialog: Dialog, user_id: int = None) -> int:
-        if user_id is None:
-            user_id = self.user_id
-        count = DialogMessageInfo.objects.filter(
-            message__dialog=dialog,
-            person__id=user_id,
-            unread=True
-        ).count()
-        return count
 
     def _get_interlocutor(self, dialog: Dialog, user_id: int = None) -> dict:
         if user_id is None:
             user_id = self.user_id
         members = DialogMembership.objects.filter(dialog=dialog)
         return members.exclude(person__id=user_id)[0].person
-
-    def _get_dialog_messages_with_filter(self, dialog: Dialog, filter_: str = None) -> DialogMessage:
-        if filter_ is not None:
-            user = User.objects.get(id=self.user_id)
-            if filter_ == "stared":
-                info = DialogMessageInfo.objects.filter(person=user, stared=True)
-                messages = DialogMessage.objects.filter(dialog=dialog, message_info__in=info)
-            elif filter_ == "unread":
-                info = DialogMessageInfo.objects.filter(person=user, unread=True)
-                messages = DialogMessage.objects.filter(dialog=dialog, message_info__in=info)
-        else:
-            messages = dialog.messages.all()
-        return messages
-
-    def _get_serialized_message(self, instance: DialogMessage, user_id: int = None, many: bool = False) -> dict:
-        if user_id is None:
-            user_id = self.user_id
-        if many:
-            messages = DialogMessageSerializer(instance, many=True).data
-            for message in messages:
-                info = DialogMessageInfo.objects.get(person__id=user_id, message__id=message["id"])
-                message["unread"] = info.unread
-                message["stared"] = info.stared
-            return messages
-        else:
-            message = DialogMessageSerializer(instance).data
-            info = DialogMessageInfo.objects.get(person__id=user_id, message=instance)
-            message["unread"] = info.unread
-            message["stared"] = info.stared
-            return message
-
-    @database_sync_to_async
-    def list(self, filter_=None) -> dict:
-        dialogs = Dialog.objects.filter(members__exact=self.user_id)
-        return [async_to_sync(self.get)(dialog.id, filter_=filter_) for dialog in dialogs]
 
     @database_sync_to_async
     def create(self, interlocutor_id: id) -> (dict, bool):
@@ -113,77 +64,3 @@ class DialogView:
             return data, True
         else:
             return {"detail": form.errors["__all__"][0]}, False
-
-    @database_sync_to_async
-    def delete(self, id_: int) -> (dict, bool):
-        try:
-            dialog = Dialog.objects.get(id=id_)
-            dialog.delete()
-            return {"id": id_}, True
-        except ObjectDoesNotExist:
-            return {"detail": "dialog doesn't exist"}, False
-
-    @database_sync_to_async
-    def send_message(self, dialog_id: int, text: str) -> (dict, bool):
-        message_data = {
-            "dialog": dialog_id,
-            "sender": self.user_id,
-            "text": text
-        }
-        form = DialogMessageForm(message_data)
-        if form.is_valid():
-            message = form.save()
-            data = self._get_serialized_message(message)
-            return data, True
-        else:
-            return {"detail": form.errors["__all__"][0]}, False
-
-    @database_sync_to_async
-    def delete_message(self, message_id: int) -> (dict, bool):
-        try:
-            message = DialogMessage.objects.get(id=message_id)
-        except ObjectDoesNotExist:
-            return {"detail": "Message doesn't exist"}, False
-        if message.sender.id != self.user_id:
-            return {"detail": "You can't delete foreign message"}, False
-        chat_id = message.dialog.id
-        message.delete()
-        return {"chat_id": chat_id, "message_id": message_id}, True
-
-    @database_sync_to_async
-    def update_message(self, message_id: int, new_text: str) -> (dict, bool):
-        try:
-            message = DialogMessage.objects.get(id=message_id)
-        except ObjectDoesNotExist:
-            return {"detail": "Message doesn't exist"}, False
-        if message.sender.id != self.user_id:
-            return {"detail": "You can't update foreign messages"}, False
-
-        form = DialogMessageForm(instance=message)
-        new_message = form.save(commit=False)
-        new_message.text = new_text
-        new_message.save()
-        return self._get_serialized_message(new_message), True
-
-    @database_sync_to_async
-    def star_message(self, message_id: int, stared: bool):
-        try:
-            info = DialogMessageInfo.objects.get(
-                message__id=message_id,
-                person__id=self.user_id,
-            )
-        except ObjectDoesNotExist:
-            return {"detail": "Message doesn't exist"}, False
-        info.stared = stared
-        info.save()
-        return {"id": message_id, "stared": stared}, True
-
-    @database_sync_to_async
-    def set_as_read(self, messages: list) -> None:
-        for message in messages:
-            info = DialogMessageInfo.objects.get(
-                person=self.user_id,
-                message__id=message["message_id"]
-            )
-            info.unread = False
-            info.save()
--- backend/groups/views.py
+++ backend/groups/views.py
@@ -6,88 +6,39 @@ from asgiref.sync import async_to_sync
 from backend.api.v1.groups.serializers import GroupSerializer, LastMessageSerializer, GroupMessageSerializer, MemberSerializer
 from backend.groups.models import ChatGroup, GroupMessage, GroupMessageInfo, GroupMembership
 from backend.groups.forms import GroupForm, GroupMessageForm, GroupMembershipForm
+from backend.socket_chat.mixins.view import ChatViewMixin
 
 
 User = get_user_model()
 
 
-class GroupView:
-    user_id = None
-
-    def set_user_id(self, user_id: int):
-        self.user_id = user_id
+class GroupView(ChatViewMixin):
+    class Meta:
+        chat_model = ChatGroup
+        chat_serializer = GroupSerializer
+        chat_form = GroupForm
+        chat_message_model = GroupMessage
+        chat_message_serializer = GroupMessageSerializer
+        chat_message_form = GroupMessageForm
+        chat_membership_model = GroupMembership
+        chat_message_info_model = GroupMessageInfo
+        last_message_serializer = LastMessageSerializer
+        chat_name = "group"
 
     @database_sync_to_async
     def get(self, id_: int, with_messages: bool = False, filter_: str = None, user_id: int = None) -> dict:
         if user_id is None:
             user_id = self.user_id
+        data = async_to_sync(super().get)(id_, with_messages, filter_, user_id)
+
         group = ChatGroup.objects.get(id=id_)
-        data = GroupSerializer(group).data
-
-        last_message = self._get_last_message(group)
-        data["last_message"] = LastMessageSerializer(last_message).data
-
-        data["unread_count"] = self._get_unread_count(group, user_id=user_id)
-
         members = self._get_members(group)
         data["members"] = MemberSerializer(members, many=True).data
 
-        messages = []
-        if with_messages:
-            messages = self._get_group_messages_with_filter(group, filter_=filter_)
-        data["messages"] = self._get_serialized_message(messages, many=True, user_id=user_id)
         return data
-
-    def _get_last_message(self, group: ChatGroup) -> GroupMessage:
-        return group.messages.last()
-
-    def _get_unread_count(self, group: ChatGroup, user_id: int = None) -> int:
-        if user_id is None:
-            user_id = self.user_id
-        count = GroupMessageInfo.objects.filter(
-            message__group=group,
-            person__id=user_id,
-            unread=True
-        ).count()
-        return count
 
     def _get_members(self, group: ChatGroup) -> dict:
         return GroupMembership.objects.filter(group=group)
-
-    def _get_group_messages_with_filter(self, group: ChatGroup, filter_: str = None) -> GroupMessage:
-        if filter_ is not None:
-            user = User.objects.get(id=self.user_id)
-            if filter_ == "stared":
-                info = GroupMessageInfo.objects.filter(person=user, stared=True)
-                messages = GroupMessage.objects.filter(group=group, message_info__in=info)
-            elif filter_ == "unread":
-                info = GroupMessageInfo.objects.filter(person=user, unread=True)
-                messages = GroupMessage.objects.filter(group=group, message_info__in=info)
-        else:
-            messages = group.messages.all()
-        return messages
-
-    def _get_serialized_message(self, instance: GroupMessage, user_id: int = None, many: bool = False) -> dict:
-        if user_id is None:
-            user_id = self.user_id
-        if many:
-            messages = GroupMessageSerializer(instance, many=True).data
-            for message in messages:
-                info = GroupMessageInfo.objects.get(person__id=user_id, message__id=message["id"])
-                message["unread"] = info.unread
-                message["stared"] = info.stared
-            return messages
-        else:
-            message = GroupMessageSerializer(instance).data
-            info = GroupMessageInfo.objects.get(person__id=user_id, message=instance)
-            message["unread"] = info.unread
-            message["stared"] = info.stared
-            return message
-
-    @database_sync_to_async
-    def list(self, filter_=None) -> dict:
-        groups = ChatGroup.objects.filter(members__exact=self.user_id)
-        return [async_to_sync(self.get)(group.id, filter_=filter_) for group in groups]
 
     @database_sync_to_async
     def create(self, name: str, slug: str, description: str = None) -> (dict, bool):
@@ -129,77 +80,3 @@ class GroupView:
             return data, True
         else:
             return {"detail": form.errors["__all__"][0]}, False
-
-    @database_sync_to_async
-    def delete(self, id_: int) -> (dict, bool):
-        try:
-            group = ChatGroup.objects.get(id=id_)
-            group.delete()
-            return {"id": id_}, True
-        except ObjectDoesNotExist:
-            return {"detail": "group doesn't exist"}, False
-
-    @database_sync_to_async
-    def send_message(self, group_id: int, text: str) -> (dict, bool):
-        message_data = {
-            "group": group_id,
-            "sender": self.user_id,
-            "text": text
-        }
-        form = GroupMessageForm(message_data)
-        if form.is_valid():
-            message = form.save()
-            data = self._get_serialized_message(message)
-            return data, True
-        else:
-            return {"detail": form.errors["__all__"][0]}, False
-
-    @database_sync_to_async
-    def delete_message(self, message_id: int) -> (dict, bool):
-        try:
-            message = GroupMessage.objects.get(id=message_id)
-        except ObjectDoesNotExist:
-            return {"detail": "Message doesn't exist"}, False
-        if message.sender.id != self.user_id:
-            return {"detail": "You can't delete foreign message"}, False
-        chat_id = message.group.id
-        message.delete()
-        return {"chat_id": chat_id, "message_id": message_id}, True
-
-    @database_sync_to_async
-    def update_message(self, message_id: int, new_text: str) -> (dict, bool):
-        try:
-            message = GroupMessage.objects.get(id=message_id)
-        except ObjectDoesNotExist:
-            return {"detail": "Message doesn't exist"}, False
-        if message.sender.id != self.user_id:
-            return {"detail": "You can't update foreign messages"}, False
-
-        form = GroupMessageForm(instance=message)
-        new_message = form.save(commit=False)
-        new_message.text = new_text
-        new_message.save()
-        return self._get_serialized_message(new_message), True
-
-    @database_sync_to_async
-    def star_message(self, message_id: int, stared: bool):
-        try:
-            info = GroupMessageInfo.objects.get(
-                message__id=message_id,
-                person__id=self.user_id,
-            )
-        except ObjectDoesNotExist:
-            return {"detail": "Message doesn't exist"}, False
-        info.stared = stared
-        info.save()
-        return {"id": message_id, "stared": stared}, True
-
-    @database_sync_to_async
-    def set_as_read(self, messages: list) -> None:
-        for message in messages:
-            info = GroupMessageInfo.objects.get(
-                person=self.user_id,
-                message__id=message["message_id"]
-            )
-            info.unread = False
-            info.save()
